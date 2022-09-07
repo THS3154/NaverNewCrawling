05966f21975f23fdba934b908f1e228a47d29894
TSH3154 <dldydwnsgud@naver.com>
2022-09-07 17:06:22 +0900

    First Crawling

---
+++ Crawling/Search.Designer.cs
@@ -0,0 +1,142 @@
+﻿
+namespace Crawling
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.TxtKeyword = new System.Windows.Forms.TextBox();
+            this.BtnKeyword = new System.Windows.Forms.Button();
+            this.BtnDBRefresh = new System.Windows.Forms.Button();
+            this.LbSearchList = new System.Windows.Forms.ListBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.LbTitles = new System.Windows.Forms.ListBox();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // TxtKeyword
+            // 
+            this.TxtKeyword.Location = new System.Drawing.Point(13, 13);
+            this.TxtKeyword.Name = "TxtKeyword";
+            this.TxtKeyword.Size = new System.Drawing.Size(434, 21);
+            this.TxtKeyword.TabIndex = 0;
+            this.TxtKeyword.KeyUp += new System.Windows.Forms.KeyEventHandler(this.TxtKeyword_KeyUp);
+            // 
+            // BtnKeyword
+            // 
+            this.BtnKeyword.Location = new System.Drawing.Point(459, 11);
+            this.BtnKeyword.Name = "BtnKeyword";
+            this.BtnKeyword.Size = new System.Drawing.Size(75, 23);
+            this.BtnKeyword.TabIndex = 1;
+            this.BtnKeyword.Text = "키워드";
+            this.BtnKeyword.UseVisualStyleBackColor = true;
+            this.BtnKeyword.Click += new System.EventHandler(this.BtnKeyword_Click);
+            // 
+            // BtnDBRefresh
+            // 
+            this.BtnDBRefresh.Location = new System.Drawing.Point(6, 258);
+            this.BtnDBRefresh.Name = "BtnDBRefresh";
+            this.BtnDBRefresh.Size = new System.Drawing.Size(163, 23);
+            this.BtnDBRefresh.TabIndex = 2;
+            this.BtnDBRefresh.Text = "DB갱신";
+            this.BtnDBRefresh.UseVisualStyleBackColor = true;
+            this.BtnDBRefresh.Click += new System.EventHandler(this.BtnDBRefresh_Click);
+            // 
+            // LbSearchList
+            // 
+            this.LbSearchList.FormattingEnabled = true;
+            this.LbSearchList.ItemHeight = 12;
+            this.LbSearchList.Location = new System.Drawing.Point(6, 20);
+            this.LbSearchList.Name = "LbSearchList";
+            this.LbSearchList.Size = new System.Drawing.Size(163, 232);
+            this.LbSearchList.TabIndex = 3;
+            this.LbSearchList.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.LbSearchList_MouseDoubleClick);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.LbSearchList);
+            this.groupBox1.Controls.Add(this.BtnDBRefresh);
+            this.groupBox1.Location = new System.Drawing.Point(13, 42);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(180, 297);
+            this.groupBox1.TabIndex = 4;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "검색한 키워드";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.LbTitles);
+            this.groupBox2.Location = new System.Drawing.Point(199, 42);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(335, 297);
+            this.groupBox2.TabIndex = 5;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "선택한 키워드 검색 내용";
+            // 
+            // LbTitles
+            // 
+            this.LbTitles.FormattingEnabled = true;
+            this.LbTitles.ItemHeight = 12;
+            this.LbTitles.Location = new System.Drawing.Point(6, 20);
+            this.LbTitles.Name = "LbTitles";
+            this.LbTitles.Size = new System.Drawing.Size(323, 268);
+            this.LbTitles.TabIndex = 4;
+            this.LbTitles.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.LbTitles_MouseDoubleClick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(542, 349);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.BtnKeyword);
+            this.Controls.Add(this.TxtKeyword);
+            this.Name = "Form1";
+            this.Text = "Search";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox TxtKeyword;
+        private System.Windows.Forms.Button BtnKeyword;
+        private System.Windows.Forms.Button BtnDBRefresh;
+        private System.Windows.Forms.ListBox LbSearchList;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.ListBox LbTitles;
+    }
+}
+
---
+++ Crawling/Search.cs
@@ -0,0 +1,143 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using System.Collections.Generic;
+using System;
+using System.Windows.Forms;
+
+namespace Crawling
+{
+    public partial class Form1 : Form
+    {
+        ChromeDriver driver;
+        Mssql ms = new Mssql();
+        List<string> hrefs;//해당 사이트주소 저장
+        string SerchUrl = "https://search.naver.com/search.naver?where=news&ie=utf8&sm=nws_hty&query=";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        
+        private void ChromeDriverSetting()
+        {
+            var driverService = ChromeDriverService.CreateDefaultService();
+            var options = new ChromeOptions();
+
+            string user_agent = "--user-agent=Mozilla/5.0 (Linux; Android 9; SM-G975F) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.83 Mobile Safari/537.36";
+            //options.AddArgument(user_agent);
+
+            //options.AddArgument("headless");//브라우저 비활성화
+            options.AddArgument("disable-gpu");
+            options.AddArgument("--mute-audio");
+
+            driver = new ChromeDriver(driverService, options);
+        }
+
+        private void BtnKeyword_Click(object sender, EventArgs e)
+        {
+            Serch();
+        }
+        private void Serch()
+        {
+            //현재 테스트용으로 10페이지까지만 검색.
+            int NowPage = 1;
+            driver.Navigate().GoToUrl(SerchUrl + TxtKeyword.Text);
+            while (true)
+            {
+                WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(1));
+
+                var ui = driver.FindElement(By.XPath("//*[@id=\"main_pack\"]/section[1]/div/div[2]/ul"));
+
+                foreach (var view in ui.FindElements(By.ClassName("bx")))
+                {
+                    var _title = view.FindElement(By.ClassName("news_tit"));
+
+
+                    //추후 사이트이름으로 특정 뉴스사만 제외하고 저장할수있게 기능 추가예정. 22.09.06
+                    string title = _title.Text;//기사제목
+                    string sitename = view.FindElement(By.ClassName("thumb_box")).Text;//뉴스 홈페이지 이름
+                    string href = _title.GetAttribute("href");//기사 링크
+
+                    //DB에 데이터 저장
+                    string query = $"INSERT INTO SEARCH(KEYWORD,TITLE,HREF,SITENAME) VALUES ('{TxtKeyword.Text}','{title.Replace("'","")}','{href}','{sitename}');";
+                    ms.SendQuery(query);
+                }
+
+                var pagenumbers = driver.FindElement(By.ClassName("sc_page_inner")).FindElements(By.TagName("a"));
+                bool Checknextpage = true;//다음페이지가 없을경우 계속 True값을 유지하기때문에 자동으로 반복문이 나가진다.
+                foreach (var page in pagenumbers)
+                {
+                    if ((NowPage + 1) == Convert.ToInt32(page.Text))
+                    {
+                        Checknextpage = false;
+                        //다음페이지로
+                        NowPage++;
+
+                        //해당페이지를 클릭으로 넘어가지않고 엔터로 넘어감.
+                        //가끔씩 클릭오류가 발생하기때문에
+                        page.SendKeys(OpenQA.Selenium.Keys.Enter);
+                        break;
+                    }
+                }
+                if (NowPage > 10)
+                    break;
+
+                if (Checknextpage)
+                    break;
+            }
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            ChromeDriverSetting();
+        }
+
+        private void TxtKeyword_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == System.Windows.Forms.Keys.Enter)
+            {
+                Serch();
+            }
+        }
+
+        private void BtnDBRefresh_Click(object sender, EventArgs e)
+        {
+            LbSearchList.Items.Clear();
+            string query = "SELECT DISTINCT KEYWORD FROM SEARCH";
+            ms.ReadData(query);
+            while (ms.rdr.Read())
+            {
+                LbSearchList.Items.Add(ms.rdr["KEYWORD"].ToString());
+            }
+            ms.RdrClose();
+        }
+
+        private void LbSearchList_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            //더블클릭했을때 선택된 아이템이 없을경우 종료
+            if (((ListBox)sender).SelectedIndex == -1)
+                return;
+            LbTitles.Items.Clear();
+            string query = $"SELECT * FROM SEARCH WHERE KEYWORD = '{LbSearchList.Items[LbSearchList.SelectedIndex].ToString()}'";
+            hrefs = new List<string>();
+            ms.ReadData(query);
+            while (ms.rdr.Read())
+            {
+                LbTitles.Items.Add(ms.rdr["TITLE"].ToString());//타이틀 출력
+                hrefs.Add(ms.rdr["HREF"].ToString());//링크값 저장
+            }
+            ms.RdrClose();
+        }
+
+        private void LbTitles_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            //더블클릭했을때 선택된 아이템이 없을경우 종료
+            int index = ((ListBox)sender).SelectedIndex;
+            if ( index == -1)
+                return;
+
+            //해당 주소로 이동시킴
+            driver.Navigate().GoToUrl(hrefs[index]);
+
+        }
+    }
+}
